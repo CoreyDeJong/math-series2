be52046e49d0899a0dc3ff289cca8e946bdd20f3
CoreyDeJong <dejongcorey@gmail.com>
2020-05-12 15:34:12 -0700

    test files linked

---
+++ math_series/series.py
@@ -0,0 +1,6 @@
+
+def fibonacci(n):
+    pass
+
+if __name__ == "__main__":
+    n=int(input())
---
+++ tests/test_series.py
@@ -0,0 +1,7 @@
+
+from math_series.series import fibonacci
+
+def test_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
