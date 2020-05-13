8034496fcf93f94b6f957e7ef28dfcd1dc38535f
CoreyDeJong <dejongcorey@gmail.com>
2020-05-12 17:51:21 -0700

    lucas and fib functions are working

--- math_series/series.py
+++ math_series/series.py
@@ -1,6 +1,32 @@
 
 def fibonacci(n):
-    pass
+    """Returns nth term of from the fibonacci sequence"""
+    prev, next=(1,1)
+
+    for i in range(0, n-2):
+            prev, next= next, prev+next
+
+    return(next)
+
+def lucas(n):
+    """Returns nth term of from the lucas sequence"""
+    prev, next=(2,1)
+
+    for i in range(0, n-2):
+            prev, next= next, prev+next
+
+    return(next)
+
+
+def sum_series(n, *args):
+    if args == '':
+        fibonacci(n)
+    if args == '2,1':
+        lucas(n)
+
+
+
+
 
 if __name__ == "__main__":
-    n=int(input())
+    n = int(input())
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,7 +1,44 @@
 
 from math_series.series import fibonacci
+from math_series.series import lucas
 
-def test_one():
+
+## Fibonacci Tests
+# def test_0():
+#     actual = fibonacci(0)
+#     expected = 0
+#     assert actual == expected
+
+def test_fib1():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
+
+def test_fib2():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fib3():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+
+## Lucas Tests
+def test_lucas1():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas5():
+    actual = lucas(5)
+    expected = 7
+    assert actual == expected
+
+def test_lucas4():
+    actual = lucas(4)
+    expected = 4
+    assert actual == expected
+
+## Sum_series test ??????
